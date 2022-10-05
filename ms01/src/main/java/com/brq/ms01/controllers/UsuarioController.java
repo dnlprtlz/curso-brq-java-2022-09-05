3477cf853f63173b70621607abe6ef9226ef9561
dnlprtlz <danilo.luz_desenv@hotmail.com>
2022-10-05 19:10:02 -0300

    mess

--- ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
+++ ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package com.brq.ms01.controllers;
 import com.brq.ms01.models.UsuarioModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,15 +22,16 @@ public class UsuarioController {
      * */
     @GetMapping("usuarios")
     public ArrayList<UsuarioModel> getAllUsuarios(){
-        for (int i = 0; i < 10; i++) {
-            UsuarioModel u = new UsuarioModel(i + 1, "usuario", "usuario@mail.com");
-            usuarios.add(u);
-        }
-
+//        for (int i = 0; i < 10; i++) {
+//            UsuarioModel u = new UsuarioModel(i + 1, "usuario", "usuario@mail.com");
+//            usuarios.add(u);
+//        }
+//
         return usuarios;
     }
     @PostMapping("usuarios")
-    public String create(){
-        return "POST Usuários";
+    public UsuarioModel create(@RequestBody UsuarioModel usuario){
+        usuarios.add(usuario);
+        return usuario;
     }
 }
