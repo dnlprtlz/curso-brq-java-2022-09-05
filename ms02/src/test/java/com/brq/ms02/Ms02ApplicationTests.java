73c0a2bc9771cb629109b554cb36d345a34dc977
dnlprtlz <danilo.luz_desenv@hotmail.com>
2022-10-06 22:45:02 -0300

    2

---
+++ ms02/src/test/java/com/brq/ms02/Ms02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brq.ms02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
